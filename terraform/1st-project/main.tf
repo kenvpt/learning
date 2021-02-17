ed924585aaafa88785c4a9307a8f2a6d5fb850b6
kenvpt <vkenneth0122@gmail.com>
2021-02-17 15:43:26 +0000

    starting module

--- terraform/1st-project/main.tf
+++ terraform/1st-project/main.tf
@@ -1,11 +1,11 @@
-module "account" {
-  source = "git::https://gitlab.com/ids-inc/ids/account_services/_baseline.git//module"
+module "vpc" {
+  source = 
 
   region      = var.region
   environment = var.environment
 
   vpc_cidr               = var.vpc["vpc_cidr"][0]
-  vpc_availability_zones = var.vpc["avialability_zones"]
+  vpc_availability_zones = var.vpc["availability_zones"]
   vpc_private_subnets    = var.vpc["private_subnets"]
   vpc_public_subnets     = var.vpc["public_subnets"]
   vpc_database_subnets   = var.vpc["database_subnets"]
--- terraform/1st-project/variable.tf
+++ terraform/1st-project/variable.tf
@@ -3,10 +3,10 @@ variable "vpc"{
     type = map(list(string))
 
     default = {
-    vpc_cidr           = ["172.19.49.0/24"]
-    public_subnets     = ["172.19.49.0/27", "172.19.49.32/27", "172.19.49.64/27"]
-    private_subnets    = ["172.19.49.96/27", "172.19.49.128/27", "172.19.49.160/27"]
-    database_subnets   = ["172.19.49.192/27", "172.19.49.224/28", "172.19.49.240/28"]
-    avialability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+    vpc_cidr           = ["10.0.0.0/16"]
+    public_subnets     = ["10.0.0.0/24", "10.0.0.0/16", "10.0.0.0/16"]
+    private_subnets    = ["10.0.0.0/16", "10.0.0.0/16", "10.0.0.0/24"]
+    database_subnets   = ["10.0.0.0/24", "10.0.0.0/24","10.0.0.0/24"]
+    availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   }
 }
