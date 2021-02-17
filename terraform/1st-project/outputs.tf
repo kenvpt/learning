7876d00365f4c398c11e024456341475dfdc7f2c
kenvpt <vkenneth0122@gmail.com>
2021-02-17 16:03:27 +0000

    push

--- terraform/1st-project/outputs.tf
+++ terraform/1st-project/outputs.tf
@@ -1,27 +1,27 @@
 output "environment" {
-  value = module.account.environment
+  value = module.vpc.environment
 }
 
 output "vpc_id" {
-  value = module.account.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  value = module.account.private_subnets
+  value = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  value = module.account.public_subnets
+  value = module.vpc.public_subnets
 }
 
 output "database_subnets" {
-  value = module.account.database_subnets
+  value = module.vpc.database_subnets
 }
 
 output "route53_private_zone_id" {
-  value = module.account.route53_private_zone_id
+  value = module.vpc.route53_private_zone_id
 }
 
 output "security_groups" {
-  value = module.account.security_groups
+  value = module.vpc.security_groups
 }
